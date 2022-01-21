ad52525ba7645967608e595beb2f7d69e5f04e63
LupisSV <guadalupesv120996@outlook.com>
2022-01-20 23:43:00 -0600

    Entrega Clase 6

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -1,29 +1,10 @@
-import React, {useEffect, useState} from "react";
+import React from "react";
 import Item from "./Item";
 
 function ItemList({ items }) {
-    // const [arregloProducto, setArregloProducto] = useState([]);
-    // const [llegoPromesa, setLlegoPromesa] = useState(false);
-
-    // useEffect(() => {
-    //     const promesaProductos = new Promise((resolve, reject) => {
-    //         setTimeout(() => {
-    //             resolve(items);
-    //         }, 2000);
-    //     })
-
-    //     promesaProductos
-    //         .then( res => {
-    //             setArregloProducto(res)
-    //             setLlegoPromesa(true)
-    //         })
-    //         .catch( err => {console.log('Hubo un error al obtener el arreglo ' + err)} )
-    // });
-
     return (
         <>            
             { 
-                // (llegoPromesa) ? <Item item={arregloProducto}></Item> : null 
                 <Item item={items}></Item>
             }
         </>
